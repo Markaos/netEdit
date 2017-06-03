af1c47e39a42621f4aed35189dd586ebc6314031
Markaos <markaos@email.cz>
2017-06-04 01:24:39 +0200

    Finally added network implementation for mudule common

---
+++ src/common/net.c
@@ -0,0 +1,123 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include "net.h"
+
+#define BUFFER_SIZE 4096
+
+void net_send_message(net_context *ctx, int peer, net_string message) {
+  write(peer, message.str, message.len); /* Making FD the peer identifier */
+                                         /* wasn't that bad idea, was it? */
+}
+
+int net_connect(net_context *ctx, char *hostname) {
+  int sock;
+  struct sockaddr_in name;
+  struct hostent *hostinfo;
+
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+  if(sock < 0) {
+    return -1;
+  }
+
+  hostinfo = gethostbyname(hostname);
+  if(hostinfo == NULL) {
+    return -1;
+  }
+
+  name.sin_family = AF_INET;
+  name.sin_port = htons(NET_PORT);
+  name.sin_addr = *(struct in_addr *) hostinfo->h_addr;
+
+  if(connect(sock, (struct sockaddr *) &name, sizeof(name)) < 0) {
+    return -1;
+  }
+
+  FD_ZERO(&(ctx->fds)); /* Just in case */
+  ctx->socket = sock;
+  return sock;
+}
+
+int net_prepare_server(net_context *ctx) {
+  int sock;
+  struct sockaddr_in name;
+
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+  if(sock < 0) {
+    return -1;
+  }
+
+  name.sin_family = AF_INET;
+  name.sin_port = htons(NET_PORT);
+  name.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if(bind(sock, (struct sockaddr *) &name, sizeof(name)) < 0) {
+    return -1;
+  }
+
+  ctx->socket = sock;
+
+  fd_set fds;
+  FD_ZERO(&fds);
+  FD_SET(sock, &fds);
+
+  ctx->fds = fds;
+
+  return 0;
+}
+
+net_string net_wait(net_context *ctx, int *from) {
+  fd_set fds = ctx->fds;
+  net_string out;
+  if(select(FD_SETSIZE, &fds, NULL, NULL, NULL) < 0) {
+    *from = -1;
+    out.len = 0;
+    out.str = NULL;
+    return out;
+  }
+
+  for(int i = 0; i < FD_SETSIZE; i++) {
+    if(FD_ISSET(i, &fds)) {
+      if(i == ctx->socket) {
+        struct sockaddr_in name;
+        socklen_t size;
+        int new = accept(i, (struct sockaddr *) &name, &size);
+
+        if(new >= 0) {
+          FD_SET(i, &(ctx->fds));
+        }
+
+        *from = -2;
+        out.len = 0;
+        out.str = NULL;
+        return out;
+      }
+
+      char buffer[BUFFER_SIZE];
+      int r = read(i, buffer, BUFFER_SIZE);
+      out.len = r;
+      out.str = realloc(out.str, sizeof(char) * (r + 1));
+      memcpy(out.str, &(buffer[0]), r);
+      out.str[out.len] = '\0';
+      *from = i;
+      return out;
+    }
+  }
+
+  *from = -1;
+  out.len = 0;
+  out.str = NULL;
+  return out;
+}
+
+void net_close(net_context *ctx, int peer) {
+  close(peer); /* Actually, the identifier is just a file descriptor */
+  FD_CLR(peer, &(ctx->fds));
+}
