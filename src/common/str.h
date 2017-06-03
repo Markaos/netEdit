346ce40656a834d912cdf9e7ee9c9ff27077e002
Markaos <markaos@email.cz>
2017-06-03 20:03:25 +0200

    Some comments for module common

--- src/common/str.h
+++ src/common/str.h
@@ -3,9 +3,50 @@
 
 #include "structs.h"
 
+/*
+ * Function: net_make_str_c
+ * ------------------------
+ * Make net_string from classic zero-delimited C string. Memory pointed to by
+ * "contents" will be used for net_string
+ *
+ * contents: C string to be wrapped
+ *
+ * returns: net_string representing the string
+ */
 net_string net_make_str_c(char *contents);
+
+/*
+ * Function: net_make_str
+ * ----------------------
+ * Make net_string from classic C string with known lenght. Memory pointed to
+ * by "contents" will be used in net_string
+ *
+ * contents: C string to be wrapped
+ * lenght: lenght of the string
+ *
+ * returns: net_string representing the string
+ */
 net_string net_make_str(char *contents, int lenght);
+
+/*
+ * Function: net_strlen
+ * --------------------
+ * Get lenght of given string
+ *
+ * str: string to be meassured
+ *
+ * returns: lenght of given string
+ */
 int net_strlen(net_string str);
+
+/*
+ * Function: net_str_append
+ * ------------------------
+ * Append one string to another
+ *
+ * dst: string to append to
+ * src: string to be appended
+ */
 void net_str_append(net_string *dst, net_string src);
 
 #endif
--- src/common/text.h
+++ src/common/text.h
@@ -3,8 +3,22 @@
 
 #include "structs.h"
 
+/*
+ * Function: net_text_append_line
+ * ------------------------------
+ * Append line to the end of text in given context
+ * 
+ * line: line to append
+ */
 void net_text_append_line(net_context *ctx, net_string line);
 
+/*
+ * Function: net_text_one_string
+ * -----------------------------
+ * Converts text in given contexts to one long string
+ *
+ * returns: string representing contents of current context
+ */
 net_string net_text_one_string(net_context *ctx);
 
 #endif
