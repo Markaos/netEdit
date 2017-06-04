5d2dde1f988d373df00f60ada80cca5474f3ec0b
Markaos <markaos@email.cz>
2017-06-04 21:32:31 +0200

    Added a few string functions

--- src/common/str.c
+++ src/common/str.c
@@ -21,9 +21,39 @@ int net_strlen(net_string str) {
   return str.len;
 }
 
+net_string net_substr(net_string str, off_t start, size_t len) {
+  net_string out;
+  out.len = len;
+  out.str = malloc(sizeof(char) * (len + 1));
+  memcpy(out.str, str.str + start, len);
+  out.str[len] = '\0';
+  return out;
+}
+
+void net_str_remove_from(net_string *str, off_t offset, size_t len) {
+  str->len -= len;
+  memmove(str->str + offset, str->str + offset + len, str->len - offset);
+  str->str = realloc(str->str, sizeof(char) * (str->len + 1));
+  str->str[str->len] = '\0';
+}
+
 void net_str_append(net_string *dst, net_string src) {
   dst->str = realloc(dst->str, sizeof(char) * (dst->len + src.len + 1));
   memcpy(dst->str + dst->len, src.str, src.len);
   dst->len += src.len;
   dst->str[dst->len] = '\0';
+}
+
+net_string net_str_from_raw(void *data, size_t len) {
+  net_string out;
+  out.len = len;
+  out.str = malloc(len + 1);
+  memcpy(out.str, data, len);
+  out.str[len] = '\0';
+  return out;
+}
+
+void net_str_extract_raw(net_string *str, void *data, size_t len) {
+  memcpy(data, str->str, len);
+  net_str_remove_from(str, 0, len);
 }
--- src/common/str.h
+++ src/common/str.h
@@ -40,6 +40,31 @@ net_string net_make_str(char *contents, int lenght);
 int net_strlen(net_string str);
 
 /*
+ * Function: net_substr
+ * --------------------
+ * Make a substring from string (original string will be left intact)
+ *
+ * str: string to shorten
+ * start: where the new string should start
+ * len: how long the new string should be
+ *
+ * returns: new string
+ */
+net_string net_substr(net_string str, off_t start, size_t len);
+
+/*
+ * Function: net_str_remove_from
+ * -----------------------------
+ * Remove _len_ bytes from string, starting at offset _offset_, adjacent string
+ * parts will be joined
+ *
+ * str: string to remove data from
+ * offset: where data to be deleted start
+ * len: how long the region being deleted is
+ */
+void net_str_remove_from(net_string *str, off_t offset, size_t len);
+
+/*
  * Function: net_str_append
  * ------------------------
  * Append one string to another
@@ -48,5 +73,28 @@ int net_strlen(net_string str);
  * src: string to be appended
  */
 void net_str_append(net_string *dst, net_string src);
+
+/*
+ * Function: net_str_from_raw
+ * --------------------------
+ * Save raw data into net_string
+ *
+ * data: data to be saved
+ * len: lenght of data in bytes
+ *
+ * returns: net_string representing the data
+ */
+net_string net_str_from_raw(void *data, size_t len);
+
+/*
+ * Function: net_str_extract_raw
+ * -----------------------------
+ * Extract raw data from string (data will be deleted from the string)
+ *
+ * str: string from which data should be exracted
+ * data: pointer to allocated memory where extracted data should be stored
+ * len: number of bytes to extract
+ */
+void net_str_extract_raw(net_string *str, void *data, size_t len);
 
 #endif
