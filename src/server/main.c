53277e1ff4ecfdfe06106e76e7d6f1a966246855
Markaos <markaos@email.cz>
2017-06-04 13:32:48 +0200

    Added placeholder server code

--- src/server/main.c
+++ src/server/main.c
@@ -1,8 +1,10 @@
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #include <text.h>
 #include <str.h>
+#include <net.h>
 
 #include "load.h"
 
@@ -12,6 +14,7 @@ int main(int argc, char *argv[]) {
   }
 
   net_context *ctx = malloc(sizeof(net_context));
+  net_string msg;
 
   int res = net_server_load_file(ctx, net_make_str_c(argv[1]));
   if(res < 0) {
@@ -19,9 +22,24 @@ int main(int argc, char *argv[]) {
     return -2;
   }
 
-  res = net_server_save_file(ctx, net_make_str_c(argv[2]));
+  res = net_prepare_server(ctx);
   if(res < 0) {
-    fputs("Cannot save file!", stderr);
-    return -2;
+    fputs("Cannot initiate server!\n", stderr);
+    return -3;
+  }
+
+  while(1) {
+    msg = net_wait(ctx, &res);
+    if(res < 0) {
+      continue;
+    }
+
+    printf("Message lenght: %d; Message: %s\n", msg.len, msg.str);
+
+    res = net_server_save_file(ctx, net_make_str_c(argv[2]));
+    if(res < 0) {
+      fputs("Cannot save file!", stderr);
+      return -2;
+    }
   }
 }
